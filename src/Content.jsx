c2420632600a4e9346d8e233c012b6a198e18a34
Moseddy <moseddy1@gmail.com>
2024-11-13 21:27:57 +0100

    edited the code based on the new information using the javascript array-map functionality

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Part from './Part'
+import React from "react";
+import Part from "./Part";
 
 const Content = (props) => {
+  
   return (
     <div>
-        <Part part={props.part1} exercise={props.exercise1} /> 
-        <Part part={props.part2} exercise={props.exercise2} /> 
-        <Part part={props.part3} exercise={props.exercise3} /> 
+      {props.parts.map((part, index) => (
+        <Part part={part.name} exercise={part.exercises} key={index} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
--- src/Total.jsx
+++ src/Total.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Total = (props) => {
-  return (
-    <p>
-        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
-    </p>
-  )
-}
+  return <p>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>;
+};
 
-export default Total
+export default Total;
