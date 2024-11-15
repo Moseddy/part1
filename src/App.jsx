f9de163fb5c2233d8586f1d04e728e447af3cdcc
Moseddy1 <moseddy1@gmail.com>
2024-11-15 07:52:01 +0100

    <update>: completed the unicafe assignment using components with props, conditional rendering, and display table

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import Content from "./Content"
-import Header from "./Header"
-import Total from "./Total"
+import { useState } from "react";
+
+const Heading = ({ title }) => <h2>{title}</h2>;
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Statistic = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td> 
+  </tr>
+);
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const feedback = "Give feedback";
+  const statistics = "Statistics";
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+  const total = good + neutral + bad;
+  let average = total === 0 ? "" : (good - bad) / total;
+  let positive = total === 0 ? "" : (good / total) * 100;
   return (
     <div>
-      <Header course={course} />
-      <Content 
-        parts={parts}
-      />
-      
-      <Total parts={parts}/>
-    </div>
-  )
-}
+      <Heading title={feedback} />
+      <Button onClick={handleGood} text={"good"} />
+      <Button onClick={handleNeutral} text={"neutral"} />
+      <Button onClick={handleBad} text={"bad"} />
 
-export default App
+      <Heading title={statistics} />
+      {total > 0 && (
+        <>
+          <table>
+            <tbody>
+              <Statistic text={"good"} value={good} />
+              <Statistic text={"neutral"} value={neutral} />
+              <Statistic text={"bad"} value={bad} />
+              <Statistic text={"all"} value={total} />
+              <Statistic text={"average"} value={average} />
+              <Statistic text={"positive"} value={positive + "%"} />
+            </tbody>
+          </table>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
