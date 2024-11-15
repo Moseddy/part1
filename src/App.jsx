28dc1d4732a55d796459ac562bbad40494e2b686
Moseddy1 <moseddy1@gmail.com>
2024-11-15 09:54:03 +0100

    solved the anecdote question

--- src/App.jsx
+++ src/App.jsx
@@ -6,48 +6,44 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
-const Statistic = ({ text, value }) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td> 
-  </tr>
-);
 
 const App = () => {
-  const feedback = "Give feedback";
-  const statistics = "Statistics";
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
 
-  const handleGood = () => setGood(good + 1);
-  const handleNeutral = () => setNeutral(neutral + 1);
-  const handleBad = () => setBad(bad + 1);
-  const total = good + neutral + bad;
-  let average = total === 0 ? "" : (good - bad) / total;
-  let positive = total === 0 ? "" : (good / total) * 100;
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ];
+
+  const handleRandomSelection = () => {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length);
+    console.log(randomNumber); // for debugging purposes only, remove in production code!
+    setSelected(randomNumber);
+  };
+
+  const handleVote = () => {
+    const newVotes = [...votes];
+    newVotes[selected]++;
+    setVotes(newVotes);
+  };
+  
   return (
     <div>
-      <Heading title={feedback} />
-      <Button onClick={handleGood} text={"good"} />
-      <Button onClick={handleNeutral} text={"neutral"} />
-      <Button onClick={handleBad} text={"bad"} />
-
-      <Heading title={statistics} />
-      {total > 0 && (
-        <>
-          <table>
-            <tbody>
-              <Statistic text={"good"} value={good} />
-              <Statistic text={"neutral"} value={neutral} />
-              <Statistic text={"bad"} value={bad} />
-              <Statistic text={"all"} value={total} />
-              <Statistic text={"average"} value={average} />
-              <Statistic text={"positive"} value={positive + "%"} />
-            </tbody>
-          </table>
-        </>
-      )}
+      <Heading title={"Anecdote of the Day"} />
+      {anecdotes[selected]} 
+      <p>has {votes[selected]} {votes[selected] > 1 ? "votes" : "vote"}</p>
+      <br />
+      <Button text={"vote"} onClick={handleVote} />
+      <Button text={"next anecdote"} onClick={handleRandomSelection} />
+      <Heading title={"Anecdote with the most votes"} />
+      <p>{anecdotes[votes.indexOf(Math.max(...votes))]} has {Math.max(...votes)} {Math.max(...votes) > 1 ? "votes" : "vote"} </p>
     </div>
   );
 };
